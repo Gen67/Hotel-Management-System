607918ce3beeacfb91a60f38192fcf676793844a
Michi <71011437+Gen67@users.noreply.github.com>
2026-04-20 16:04:38 +0800

    Add files via upload

--- Hotel Management System/JavaScript/loginSite.js
+++ Hotel Management System/JavaScript/loginSite.js
@@ -1,22 +1,94 @@
 const getInfo = document.querySelector('.login-layout'); 
-
-
-    const firstName = document.getElementById("firstName"); 
-    const secondName = document.getElementById("secondName").value; 
-
-    const email = document.getElementById("Email").value; 
-    const checkEmail = document.getElementById("confirmEmail").value;
-    const phone = document.getElementById("phone").value;
-
-
-    const address = document.getElementById("address").value; 
-    const city = document.getElementById("city").value; 
-    const postCode = document.getElementById("post-code").value; 
-    const country = document.getElementById("country").value; 
-
-    const survey = document.getElementById("survey").value; 
+const inputs =  document.querySelectorAll("input"); 
 
 
 
+ let accountInfo = {
+                            firstName:" ",
+                            secondName:" ",
+                            email: " ",
+                            checkEmail:" ",
+                            phone: " ", 
+                            address: " ",
+                            city:" ",
+                            postCode: " ",
+                            country: " ",
+                            survey:" "};
 
-getInfo.addEventListener("submit", event =>{event.preventDefault();}); 
+
+
+
+inputs.forEach(inputs =>{
+    inputs.addEventListener('input', function(event){
+        accountInfo[event.target.id] = event.target.value; 
+    }); 
+});
+
+
+
+getInfo.addEventListener("submit", event =>{
+     event.preventDefault();
+    createAccount();    
+
+    
+              setTimeout(() => {
+    const url = "../HTML/main.html";
+    const tab = window.open(url, "_self");
+    }, 3000); 
+  
+    
+
+
+
+}); 
+
+
+
+  function createAccount(){ 
+
+        let  {firstName, secondName, email, checkEmail,phone,address,city,postCode,country,survey} = accountInfo;
+        let accountCreateSuccessfully = false;
+
+            if(firstName === " " || secondName === " "){
+                alert("Fill the Form");
+                return false;
+            } else{ 
+                console.log(`First Name: ${firstName}`);
+                console.log(`Second Name: ${secondName}`);
+            }
+            
+            
+            if(email.includes('@') && email.includes('.')){
+                console.log(`Email: ${email}`);
+            } else{ 
+                alert("Note a Valid Email");
+                return false;
+            }
+
+            if(email.value === checkEmail.value){
+                console.log(`Email Confirm`);
+            } else{ 
+                alert("Email not The same");         
+                return false;   
+            }
+
+            if(phone === " " || address === " " || city === " " || postCode === " " || country === " "){
+                alert("Please Complete the Form"); 
+                return false;
+            } else { 
+                console.log(`Phone No: ${phone}`); 
+                console.log(`Address: ${address}`); 
+                console.log(`City: ${city}`); 
+                console.log(`Post Code: ${postCode}`); 
+                console.log(`Country: ${country}`); 
+            }
+
+                 accountCreateSuccessfully= true;
+                 console.log("Account created successfully!");
+
+            
+        return accountInfo;
+    }
+
+
+
